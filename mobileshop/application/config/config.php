9ac39e38c825eb44d0b105dd917398eb0082a592
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-03-16 19:43:02 +0530

    update

--- mobileshop/application/config/config.php
+++ mobileshop/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://mobileshop.herokuapp.com/';
+$config['base_url'] = 'https://mobileshop.herokuapp.com/mobileshop/';
 
 /*
 |--------------------------------------------------------------------------
@@ -52,7 +52,8 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+//$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']	= 'AUTO';
 
 /*
 |--------------------------------------------------------------------------
