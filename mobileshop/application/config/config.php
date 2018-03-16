92a191e781d9433b13873b74fce20d5464427456
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-03-16 19:38:35 +0530

    update

--- mobileshop/application/config/config.php
+++ mobileshop/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://mobileshop.herokuapp.com/mobileshop';
+$config['base_url'] = 'https://mobileshop.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
